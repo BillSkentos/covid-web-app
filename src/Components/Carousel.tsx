e338922d0b0f65d03ade96e8cc9fd8846f838452
Bill Skentos <vskentos1@gmail.com>
2021-04-24 23:07:05 +0300

    added carousel

---
+++ src/Components/Carousel.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
+
+const images = [
+  {url:'https://images.pexels.com/photos/3735769/pexels-photo-3735769.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+  {url:'https://images.pexels.com/photos/7605733/pexels-photo-7605733.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+  {url:'https://images.pexels.com/photos/5994798/pexels-photo-5994798.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+  {url:' https://images.pexels.com/photos/3985170/pexels-photo-3985170.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'}
+];
+
+
+
+
+export default function Carousel() {
+  return (
+    <AliceCarousel autoPlay={true}  autoPlayInterval={2500}>
+      {
+        images.map(img=>{
+          return <img src = {img.url} key={img.url} className="sliderImg" alt="carouselImg" />
+        })
+      }
+
+</AliceCarousel>
+  )
+}
+
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -3,6 +3,7 @@ import Lottie from 'react-lottie';
 import DoctorAnimation from '../Animations/53912-doctor-consultation-online-animation.json';
 import {Link} from 'react-router-dom';
 import {useAppContext} from '../Components/ContextProvider';
+import Carousel from '../Components/Carousel';
 
 export default function HomePage() {
   
@@ -18,6 +19,7 @@ export default function HomePage() {
   }
 
   return (
+    <>
     <div className = "z-10 p-4 flex lg:flex-row flex-col items-center justify-center md:mt-12 lg:mt-8">
       <div className="lg:w-1/2 w-full flex flex-col space-y-8 items-center">
         <h1 className="mt-12 text-lg sm:mt-4 font-mono sm:pl-12 sm:text-4xl text-bold text-FQAItem">Κλέισε ραντεβού για εμβολιασμό και <br/> δες πληροφορίες για τον κωροναϊό εδώ
@@ -32,6 +34,9 @@ export default function HomePage() {
         {(width <1024 && width >= 768) && <Lottie options = {defaultOptions} width={400} height={400} /> }
         {width <768 && <Lottie options = {defaultOptions} width={300} height={300} /> }
       </div>
-    </div>
-  )
+    </div> <br/>
+    <Carousel />
+    </>
+  );
+
 }
