afd42ae254b47de4a8adad250ad609788d2b5f2f
Bill Skentos <vskentos1@gmail.com>
2021-05-05 22:02:43 +0300

    fixed video player

--- src/Components/VideoSlider.js
+++ src/Components/VideoSlider.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import {useState} from 'react';
 import Carousel from "react-elastic-carousel";
-import {YoutubePlayer} from 'reactjs-media';
+import ReactPlayer from 'react-player/lazy';
+
 
 export default function VideoSlider() {
 
-  const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
 
   const links = [
     'https://www.youtube.com/watch?v=_ng_bVJMm88',
@@ -14,7 +12,7 @@ export default function VideoSlider() {
     'https://www.youtube.com/watch?v=Wk97ev-M2Nw',
     'https://www.youtube.com/watch?v=FZta8t2L1pk',
     'https://www.youtube.com/watch?v=BOmSrKRWJS0',
-  ]
+  ];
 
 
   const breakPoints  = [
@@ -30,7 +28,7 @@ export default function VideoSlider() {
         <div className="w-full carousel-wrapper">
           <Carousel breakPoints={breakPoints} isRTL ={false} >
             {links.map((item,index) => (
-                <YoutubePlayer  src="https://www.youtube.com/watch?v=5Zmx5D7dOYA&feature=emb_title"  className="flex justify-between items-center w-72  m-8"  height={350} key = {index}/>
+                <ReactPlayer className="flex justify-between items-center w-72  m-8" key = {index} url = {item}/>
             ))}
           </Carousel>
         </div>
@@ -39,3 +37,4 @@ export default function VideoSlider() {
 
 }
 
+                
